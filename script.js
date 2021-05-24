e7dc0bf570c3656b6b3b6b64491bfaa0ba93dac8
Liam Donovan <ltdonova@ncsu.edu>
2021-05-24 00:14:07 -0400

    merge

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
 var backspaceIcon = document.querySelector('.backspace');
 
+var keypadButtons = document.querySelectorAll('.keypadButton');
+
 function lightenBackspace(){
     if(true){
         backspaceIcon.classList.add('light-backspace');
     }
 }
+
+function addToDisplay(){
+    
+}
+
+
 
 function dimBackspace(){
     backspaceIcon
